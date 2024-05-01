b633b9d3235fef8d295ba5fc356c6d0ef1f0c062
MhL5 <sapumr@gmail.com>
2024-05-01 16:12:42 +0330

    update login axios error response

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -3,6 +3,7 @@ import { login } from "../services/login";
 import { showAlert } from "../utils/alert";
 import { useAuthContext } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { isAxiosError } from "axios";
 
 function useLogin() {
   const { setUserFn } = useAuthContext();
@@ -19,8 +20,13 @@ function useLogin() {
       showAlert("success", "logged in");
       navigate(-1);
     },
-    onError: () => {
-      showAlert("error", "something went wrong");
+    onError: (err) => {
+      if (isAxiosError(err))
+        showAlert(
+          "error",
+          `${err?.response?.status} ${err?.response?.data.message}`
+        );
+      else showAlert("error", `${err.name} ${err.message}`);
     },
   });
 
